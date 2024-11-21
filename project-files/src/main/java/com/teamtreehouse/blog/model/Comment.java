615a61b5fb5602d9e603352cf6d23b2cb35e8ffa
Leah Seyoum <119345274+leahseyoum@users.noreply.github.com>
2024-11-20 16:29:56 -0800

    created comment model

--- project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,17 @@
 package com.teamtreehouse.blog.model;
 
 public class Comment {
+    private String author;
+    private String content;
+    private String date;
+
+    public Comment(String author, String content, String date) {
+        this.author = author;
+        this.content = content;
+        this.date = date;
+    }
+
+    public String getAuthor() {return author;}
+    public String getContent() {return content;}
+    public String getDate() {return date;}
 }
