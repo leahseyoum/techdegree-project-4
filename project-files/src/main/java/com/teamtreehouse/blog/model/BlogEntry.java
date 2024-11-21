1cf985e12249662c4ad3c8fcd13abb82d212e2ec
Leah Seyoum <119345274+leahseyoum@users.noreply.github.com>
2024-11-20 16:43:59 -0800

    blog entry model created

--- project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,29 @@
 package com.teamtreehouse.blog.model;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlogEntry {
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime date;
+    private List<Comment> comments;
+
+    public BlogEntry(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.date = LocalDateTime.now();
+        this.comments = new ArrayList<>();
+    }
+
     public boolean addComment(Comment comment) {
-        // Store these comments!
-        return false;
+       return comments.add(comment);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
     }
 }
--- project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,17 +1,20 @@
 package com.teamtreehouse.blog.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Comment {
     private String author;
     private String content;
-    private String date;
+    private LocalDateTime date;
 
-    public Comment(String author, String content, String date) {
+    public Comment(String author, String content) {
         this.author = author;
         this.content = content;
-        this.date = date;
+        this.date = LocalDateTime.now();
     }
 
     public String getAuthor() {return author;}
     public String getContent() {return content;}
-    public String getDate() {return date;}
+
 }
